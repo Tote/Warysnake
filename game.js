1a7089f42c7f4a0c98b5df8b32d033a0c96b1264
Patricio Morel <patricio.morel@outlook.com>
2019-07-19 00:04:32 -0400

    tests loop, item and clear

--- game.js
+++ game.js
@@ -2,12 +2,37 @@
 import GameCanvas   from './gloop/GameCanvas.js'
 import Graphics     from './gloop/Graphics.js'
 import GameLoop     from './gloop/GameLoop.js'
+import Block        from './Block.js'
 
 const canvas    = new GameCanvas()
 const graphics  = new Graphics(canvas)
 const loop      = new GameLoop()
 
+
+const block = new Block()
+block.draw(graphics)
+
+
+let speed = 1
+
+loop.rule({
+    when: () => true,
+    then: () => block.x += speed
+})
+loop.rule({
+    when: () => block.x+block.w > canvas.width(),
+    then: () => speed = -10
+})
+loop.rule({
+    when: () => block.x < 0,
+    then: () => speed = 10
+})
+loop.rule({
+    when: () => true,
+    then: () => graphics.clear()
+})
+loop.rule({
+    when: () => true,
+    then: () => block.draw(graphics)
+})
 loop.run()
-
-
-graphics.fill()
