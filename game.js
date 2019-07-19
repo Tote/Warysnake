596da353ad4287f5c1b81a6d78f75efdf122df55
Patricio Morel <patricio.morel@outlook.com>
2019-07-18 23:22:45 -0400

    Moved files to 'gloop' folder

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 //Create a fullscreen game
-import GameCanvas   from './GameCanvas.js'
-import Graphics     from './Graphics.js'
-import GameLoop     from './GameLoop.js'
+import GameCanvas   from './gloop/GameCanvas.js/index.js'
+import Graphics     from './gloop/Graphics.js/index.js'
+import GameLoop     from './gloop/GameLoop.js/index.js'
 
 const canvas    = new GameCanvas()
 const graphics  = new Graphics(canvas)
