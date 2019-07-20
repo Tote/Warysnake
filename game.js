7eb260579e987cec9ecb116c134144571bb66bb3
Patricio Morel <totenbot@outlook.com>
2019-07-20 01:10:39 -0400

    game.js cleaning

--- game.js
+++ game.js
@@ -11,18 +11,7 @@ const loop      = new Gloop(graphics)
 
 const block = new Block()
 loop.item(block)
-let speed = 1
 
-loop.rule({
-    when: () => true,
-    then: () => block.x += speed
-})
-loop.rule({
-    when: () => block.x+block.w > canvas.width(),
-    then: () => speed = -10
-})
-loop.rule({
-    when: () => block.x < 0,
-    then: () => speed = 10
-})
+
+
 loop.run()
