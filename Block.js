56b46c50c54fc39216874529edb49efb4b6f00ab
Patricio Morel <totenbot@outlook.com>
2019-07-20 01:11:16 -0400

    Implemented next() in Block example

--- Block.js
+++ Block.js
@@ -7,9 +7,14 @@ export default class Block extends GloopItem{
         this.y = 0
         this.w = 50
         this.h = 50
+        this.vx = 10
     }
     
     draw(graphics){
         graphics.rect(this)
+    }
+
+    next(){
+        this.x += this.vx
     }
 }
