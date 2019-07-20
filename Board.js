2646f36f1b970f312097f59e62ac9a1767f46bbc
Patricio Morel <totenbot@outlook.com>
2019-07-20 01:37:00 -0400

    Draw a grid for the board

---
+++ Board.js
@@ -0,0 +1,27 @@
+import GloopItem from "./gloop/GloopItem.js";
+
+export default class Board extends GloopItem{
+    constructor(rows, cols, width, height){
+        super()
+        this.rowCount   = rows
+        this.colCount   = cols
+        this.width      = width
+        this.height     = height
+        this.rowHeight  = height / rows
+        this.colWidth   = width / cols
+    }
+    draw(g){
+        const c = g.context;
+        c.beginPath()
+        c.moveTo(0,0)
+        for(let r = 0; r < this.height; r+=this.rowHeight){
+            c.moveTo(0,r)
+            c.lineTo(this.width,r)
+        }
+        for(let l = 0; l < this.width; l+=this.colWidth){
+            c.moveTo(l,0)
+            c.lineTo(l,this.height)
+        }
+        c.stroke()
+    }
+}
--- game.js
+++ game.js
@@ -1,17 +1,14 @@
-//Create a fullscreen game
 import GameCanvas   from './gloop/GameCanvas.js'
 import Graphics     from './gloop/Graphics.js'
-import Gloop     from './gloop/Gloop.js'
-import Block        from './Block.js'
+import Gloop        from './gloop/Gloop.js'
+
+import Board    from './Board.js'
 
 const canvas    = new GameCanvas()
 const graphics  = new Graphics(canvas)
 const loop      = new Gloop(graphics)
 
-
-const block = new Block()
-loop.item(block)
-
-
+const board = new Board(10,10,canvas.width(), canvas.height())
+loop.item(board)
 
 loop.run()
