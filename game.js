ed9476f044fcf84dc593a975424aa5c96100fc69
Patricio Morel <patricio.morel@outlook.com>
2019-07-22 02:47:18 -0400

    [game] test with a second snake

--- game.js
+++ game.js
@@ -19,13 +19,22 @@ let fruit = BoardCell.random(board)
 fruit.color = 'red'
 
 const snake = new Snake()
+snake.color = 'green'
 snake.addToBody(new BoardCell(0,0,board))
 snake.addToBody(new BoardCell(1,0,board))
 snake.addToBody(new BoardCell(2,0,board))
 snake.goRight()
 
+const snake2 = new Snake()
+snake2.color = 'purple'
+snake2.addToBody(new BoardCell(9,9,board))
+snake2.addToBody(new BoardCell(8,9,board))
+snake2.addToBody(new BoardCell(7,9,board))
+snake2.goLeft()
+
 loop.item(fruit)
 loop.item(snake)
+loop.item(snake2)
 
 keyboard.press({
     when: 'ArrowUp',
@@ -44,6 +53,23 @@ keyboard.press({
     then: () => snake.goLeft()
 })
 
+keyboard.press({
+    when: 'w',
+    then: () => snake2.goUp()
+})
+keyboard.press({
+    when: 'd',
+    then: () => snake2.goRight()
+})
+keyboard.press({
+    when: 's',
+    then: () => snake2.goDown()
+})
+keyboard.press({
+    when: 'a',
+    then: () => snake2.goLeft()
+})
+
 loop.rule({
     when: () => snake.eats(fruit),
     then: () => {
@@ -56,6 +82,18 @@ loop.rule({
 })
 
 loop.rule({
+    when: () => snake2.eats(fruit),
+    then: () => {
+        snake2.addToBody(fruit);
+        fruit = BoardCell.random(board)
+        fruit.color = 'red'
+        loop.item(fruit)
+        snake2.speed++
+    }
+})
+
+
+loop.rule({
     when: () => snake.eats(board.walls),
     then: () => snake.stop()
 })
@@ -63,5 +101,15 @@ loop.rule({
 loop.rule({
     when: () => snake.eats(snake.body),
     then: () => snake.stop()
+})
+
+loop.rule({
+    when: () => snake2.eats(board.walls),
+    then: () => snake2.stop()
+})
+
+loop.rule({
+    when: () => snake2.eats(snake2.body),
+    then: () => snake2.stop()
 })
 loop.run()
