a3372d0ac0d1a7c571d98a73c8c0333d23fcc4f5
Patricio Morel <patricio.morel@outlook.com>
2019-07-23 23:19:31 -0400

    Refactor and order

--- game.js
+++ game.js
@@ -1,75 +1,78 @@
 import GameCanvas   from './gloop/GameCanvas.js'
 import Graphics     from './gloop/Graphics.js'
 import Gloop        from './gloop/Gloop.js'
-import Keys         from './gloop/Keys.js'
 
 import Board        from './Items/Board.js'
 import BoardCell    from './Items/BoardCell.js'
 import Snake        from './Items/Snake.js'
+import BrowserEvent from './gloop/BrowserEvent.js';
 
 const canvas    = new GameCanvas()
 const graphics  = new Graphics(canvas)
 const loop      = new Gloop(graphics)
-const keyboard  = new Keys()
+const keyboard  = new BrowserEvent('keydown')
+const touch     = new BrowserEvent('touchstart')
 
-const board = new Board(10,10,canvas.width(), canvas.height())
+
+/**********************************
+ *  OBJECTS
+ **********************************/
+const board = new Board(20,50,canvas.width(), canvas.height())
 loop.item(board)
 
-let fruit = BoardCell.random(board)
-fruit.color = 'red'
-
 const snake = new Snake()
-snake.color = 'green'
 snake.addToBody(new BoardCell(0,0,board))
 snake.addToBody(new BoardCell(1,0,board))
 snake.addToBody(new BoardCell(2,0,board))
 snake.goRight()
-
-const snake2 = new Snake()
-snake2.color = 'purple'
-snake2.addToBody(new BoardCell(9,9,board))
-snake2.addToBody(new BoardCell(8,9,board))
-snake2.addToBody(new BoardCell(7,9,board))
-snake2.goLeft()
-
-loop.item(fruit)
 loop.item(snake)
-loop.item(snake2)
 
-keyboard.press({
-    when: 'ArrowUp',
+let fruit = BoardCell.random(board)
+fruit.color = 'red'
+loop.item(fruit)
+
+
+/**********************************
+ *  CONTROLS
+ **********************************/
+keyboard.rule({
+    when:  e => e.key == 'ArrowUp',
     then: () => snake.goUp()
 })
-keyboard.press({
-    when: 'ArrowRight',
+keyboard.rule({
+    when: e => e.key == 'ArrowRight',
     then: () => snake.goRight()
 })
-keyboard.press({
-    when: 'ArrowDown',
+keyboard.rule({
+    when: e => e.key == 'ArrowDown',
     then: () => snake.goDown()
 })
-keyboard.press({
-    when: 'ArrowLeft',
+keyboard.rule({
+    when: e => e.key == 'ArrowLeft',
     then: () => snake.goLeft()
 })
 
-keyboard.press({
-    when: 'w',
-    then: () => snake2.goUp()
+touch.preprocess = e => e.touches[0]
+touch.rule({
+    when: e => e.clientX > canvas.width()/2 && snake.isMovingVertically(),
+    then: e => snake.goRight()
 })
-keyboard.press({
-    when: 'd',
-    then: () => snake2.goRight()
+touch.rule({
+    when: e => e.clientX < canvas.width()/2 && snake.isMovingVertically(),
+    then: e => snake.goLeft()
 })
-keyboard.press({
-    when: 's',
-    then: () => snake2.goDown()
+touch.rule({
+    when: e => e.clientY > canvas.height()/2 && snake.isMovingHorizontally(),
+    then: e => snake.goDown()
 })
-keyboard.press({
-    when: 'a',
-    then: () => snake2.goLeft()
+touch.rule({
+    when: e => e.clientY < canvas.height()/2 && snake.isMovingHorizontally(),
+    then: e => snake.goUp()
 })
 
+/**********************************
+ *  RULES
+ **********************************/
 loop.rule({
     when: () => snake.eats(fruit),
     then: () => {
@@ -82,18 +85,6 @@ loop.rule({
 })
 
 loop.rule({
-    when: () => snake2.eats(fruit),
-    then: () => {
-        snake2.addToBody(fruit);
-        fruit = BoardCell.random(board)
-        fruit.color = 'red'
-        loop.item(fruit)
-        snake2.speed++
-    }
-})
-
-
-loop.rule({
     when: () => snake.eats(board.walls),
     then: () => snake.stop()
 })
@@ -101,15 +92,5 @@ loop.rule({
 loop.rule({
     when: () => snake.eats(snake.body),
     then: () => snake.stop()
-})
-
-loop.rule({
-    when: () => snake2.eats(board.walls),
-    then: () => snake2.stop()
-})
-
-loop.rule({
-    when: () => snake2.eats(snake2.body),
-    then: () => snake2.stop()
 })
 loop.run()
